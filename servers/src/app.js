3561d98e1f7c0435b0f48a7baed0968997e650de
Nitiphumi Intakanok <bkzmatokung@gmail.com>
2023-08-09 11:23:56 +0700

    'pg54'

--- servers/src/app.js
+++ servers/src/app.js
@@ -2,7 +2,12 @@ let express = require('express')
 const app = express()
 
 app.get('/status', function (req,res){
-    res.send('Hello nodejs server 1234')
+    res.send('Hello nodejs server belong to nitiphumi')
+})
+
+app.get('/hello/:name', function(req,res){
+    console.log('hello - ' + req.params.name)
+    res.send('say hello with ' + req.params.name)
 })
 
 let port = 8081
